0372ee23ffcd2a65a036be826bdfc834c684741a
carlosrelter <reltercontato@gmail.com>
2025-05-21 16:59:07 -0300

    commit initial Client API

---
+++ src/test/java/com/github/carlosrelter/clientAPI/ClientApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.carlosrelter.clientAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
